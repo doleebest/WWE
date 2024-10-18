26e43fed3a52c207e14b71a7279df4a7878194f9
CSE-pebble <CSE.pebble@gmail.com>
2024-10-18 23:32:28 +0900

    Feat: 해당 페이지의 네비게이션 아이템 강조

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,11 +1,32 @@
 export default function Navbar() {
+  const currentPath = window.location.pathname;
+  const formattedPath = currentPath.replace(/\/src\/pages/, "");
+
+  // 필요하면 이후 수정
+  const menuItems = [
+    { text: "홈", href: "/index.html" },
+    { text: "상품 등록", href: "/reg_items.html" },
+    { text: "마이페이지", href: "/mypage.html" },
+  ];
+
+  const menuHtml = menuItems
+    .map(
+      (item) => `
+        <li>
+          <a href="${item.href}" style="color: ${
+        item.href === formattedPath ? "#00462a" : "black"
+      }; text-decoration: ${
+        item.href === formattedPath ? "underline" : "none"
+      }">${item.text}</a>
+        </li>`
+    )
+    .join("");
+
   return {
     element: `<nav class="navbar">
-    <ul class="navbar__menu">
-      <li><a href="/">홈</a></li>
-      <li><a href="/reg_items">상품 등록</a></li>
-      <li><a href="/mypage">마이페이지</a></li>
-    </ul>
-  </nav>`,
+        <ul class="navbar__menu">
+          ${menuHtml}
+        </ul>
+      </nav>`,
   };
 }
