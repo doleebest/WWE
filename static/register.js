48c36b681140621599f3259c8f63ee2e905dbaa5
arky02 <kyean07@naver.com>
2024-11-10 00:59:54 +0900

    fix:form tag button 값 전송 안되는 오류 수정

--- static/register.js
+++ static/register.js
@@ -1,13 +1,10 @@
-const setRegNavBar = () => {
-  const regItems = document.querySelector("#navbar__menu__reg_items");
-  regItems.classList.add("navbar__menu_selected");
-};
-
 const setContent = () => {
   const continentButtons = document.querySelectorAll(".continent-btn");
   const stateButtons = document.querySelectorAll(".state-btn");
 
-  console.log(continentButtons);
+  const continentInput = document.getElementById("continentInput");
+  const stateInput = document.getElementById("stateInput");
+
   continentButtons.forEach((button) => {
     button.addEventListener("click", () => {
       // 모든 버튼에서 selected 클래스 제거
@@ -16,8 +13,9 @@ const setContent = () => {
       // 클릭한 버튼에만 selected 클래스 추가
       button.classList.add("selected");
 
-      // 선택한 대륙 값 가져오기
+      // 선택한 대륙 값을 숨겨진 input에 넣기
       const selectedContinent = button.dataset.continent;
+      continentInput.value = selectedContinent; // 숨겨진 input에 선택한 값 저장
       console.log("선택한 대륙:", selectedContinent);
     });
   });
@@ -30,17 +28,10 @@ const setContent = () => {
       // 클릭한 버튼에만 selected 클래스 추가
       button.classList.add("selected");
 
-      // 선택한 상품 상태 값 가져오기
+      // 선택한 상품 상태 값을 숨겨진 input에 넣기
       const selectedState = button.dataset.state;
+      stateInput.value = selectedState; // 숨겨진 input에 선택한 값 저장
       console.log("선택한 상품 상태:", selectedState);
     });
   });
-
 };
-
-const init = () => {
-  setNavBar = setRegNavBar; // 재정의
-  setContent();
-};
-
-init();
