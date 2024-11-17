97cd2ba65db9f04f028d3f8d9c3d4831c1a23cfd
nakyeong <rinarina0429@naver.com>
2024-11-17 23:04:39 +0900

    FEAT: db에서 상품 이름으로 상품 조회하기 기능 추가

--- database.py
+++ database.py
@@ -30,6 +30,16 @@ class DBhandler:
     def get_items(self):
         items = self.db.child("item").get().val()
         return items
+    
+    def get_item_byname(self, name):
+        items = self.db.child("item").get()
+        target_value=""
+        print("###########",name)
+        for res in items.each():
+            key_value = res.key()
+            if key_value == name:
+                target_value=res.val()
+        return target_value
 
     def insert_user(self, data, pw) :
          user_info = {
