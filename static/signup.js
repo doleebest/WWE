9196d17653538ead09069b6d32e9f1ed32b59df3
CSE-pebble <CSE.pebble@gmail.com>
2024-11-22 16:59:11 +0900

    Feat: 회원가입 버튼 활성화/비활성화 로직 구현

--- static/signup.js
+++ static/signup.js
@@ -14,6 +14,10 @@ const emailAlert = document.getElementById("email_alert");
 // 휴대전화 관련 element
 const phoneInput = document.getElementById("phone");
 const phoneAlert = document.getElementById("phone_alert");
+// 관심 지역 관련 element
+const region = document.getElementById("region");
+// 회원가입 하기 버튼
+const signupBtn = document.querySelector(".signup_btn");
 
 // 정규식
 const regExp = {
@@ -33,11 +37,20 @@ const validationFlag = {
 };
 
 const checkIdValidation = () => {
+  validationFlag.id = false;
+  duplicateCheckBtn.disabled = true;
+  if (idInput.value.length == 0) {
+    idAlert.classList.add("none");
+    checkAllValidation();
+    return;
+  }
   idAlert.classList.remove("none");
   if (regExp.id.test(idInput.value)) {
     duplicateCheckBtn.classList.remove("btn_deactivate");
     duplicateCheckBtn.classList.add("btn_activate");
+    duplicateCheckBtn.disabled = false;
     idAlert.classList.add("none");
+    validationFlag.id = true;
   } else {
     duplicateCheckBtn.classList.remove("btn_activate");
     duplicateCheckBtn.classList.add("btn_deactivate");
@@ -45,53 +58,103 @@ const checkIdValidation = () => {
     idAlert.textContent =
       "아이디는 영어 소문자로 시작하는 영문, 숫자 조합으로 5~15자 이내 입력해주세요.";
   }
+  checkAllValidation();
 };
 
 const checkPwValidation = () => {
+  validationFlag.pw = false;
+  if (pwInput.value.length == 0) {
+    checkPwReValidation();
+    pwAlert.classList.add("none");
+    checkAllValidation();
+    return;
+  }
   pwAlert.classList.remove("none");
+
   if (pwReInput.value.length > 0) {
     checkPwReValidation();
   }
 
   if (regExp.pw.test(pwInput.value)) {
     pwAlert.classList.add("none");
+    validationFlag.pw = true;
   } else {
     pwAlert.classList.add("alert_bad");
     pwAlert.textContent =
       "비밀번호는 영문, 숫자, 특수문자 조합으로 8자 이상 입력해주세요.";
   }
+  checkAllValidation();
 };
 
 const checkPwReValidation = () => {
+  validationFlag.pwRe = false;
+  if (pwReInput.value.length == 0) {
+    pwReAlert.classList.add("none");
+    checkAllValidation();
+    return;
+  }
   pwReAlert.classList.remove("none");
   if (pwReInput.value === pwInput.value) {
     pwReAlert.classList.remove("alert_bad");
     pwReAlert.classList.add("alert_good");
     pwReAlert.textContent = "비밀번호가 일치합니다.";
+    validationFlag.pwRe = true;
   } else {
     pwReAlert.classList.remove("alert_good");
     pwReAlert.classList.add("alert_bad");
     pwReAlert.textContent = "비밀번호가 일치하지 않습니다.";
   }
+  checkAllValidation();
 };
 
 const checkEmailValidation = () => {
+  validationFlag.email = false;
+  if (emailInput.value.length == 0) {
+    emailAlert.classList.add("none");
+    checkAllValidation();
+    return;
+  }
   emailAlert.classList.remove("none");
   if (regExp.email.test(emailInput.value)) {
     emailAlert.classList.add("none");
+    validationFlag.email = true;
   } else {
     emailAlert.classList.add("alert_bad");
     emailAlert.textContent = "이메일 형식을 지켜서 입력해주세요.";
   }
+  checkAllValidation();
 };
 
 const checkPhoneValidation = () => {
+  if (phoneInput.value.length == 0) {
+    phoneAlert.classList.add("none");
+    validationFlag.phone = true; // 휴대전화는 optional이므로
+    checkAllValidation();
+    return;
+  }
+  validationFlag.phone = false;
   phoneAlert.classList.remove("none");
   if (regExp.phone.test(phoneInput.value)) {
     phoneAlert.classList.add("none");
+    validationFlag.phone = true;
   } else {
     phoneAlert.classList.add("alert_bad");
-    phoneAlert.textContent = "010-XXXX-XXXX 형식으로 입력해주세요.";
+    phoneAlert.textContent = "'010-XXXX-XXXX' 형식으로 입력해주세요.";
+  }
+  checkAllValidation();
+};
+
+const checkAllValidation = () => {
+  const allValid = Object.values(validationFlag).every((flag) => flag);
+
+  if (allValid) {
+    signupBtn.classList.remove("btn_deactivate");
+    signupBtn.classList.add("signup_btn_activate");
+    signupBtn.disabled = false;
+  } else {
+    signupBtn.classList.remove("signup_btn_activate");
+    signupBtn.classList.add("btn_deactivate");
+    signupBtn.disabled = true;
   }
 };
 
@@ -101,14 +164,22 @@ pwReInput.addEventListener("input", checkPwReValidation);
 emailInput.addEventListener("input", checkEmailValidation);
 phoneInput.addEventListener("input", checkPhoneValidation);
 
+// 임시
+duplicateCheckBtn.addEventListener("click", () => {
+  console.log("중복 확인 버튼 클릭");
+});
+signupBtn.addEventListener("click", () => {
+  console.log("회원가입 버튼 클릭");
+});
+
 /* TODO
 0. 아이디 중복 체크 API 연결
    - 프론트에서도 중복 체크 여부 저장하는 변수 생성 필요
    - 중복 체크를 했더라도 id input란에 변경이 생기면 다시 중복 체크 여부 저장하는 변수 false
    - 중복 체크 true가 되었다면 중복 확인 버튼은 비활성화
 1. validationFlag가 모두 true가 되었을 때만 회원가입 하기 버튼 활성화
-   - 중복 체크 여부와 상관없이 우선 id input이 validate 한지만 판단
-   - 휴대전화와 관심 지역은 값을 입력했을 경우에만 flag를 false로 바꾸고 validation 체크
+   완) 중복 체크 여부와 상관없이 우선 id input이 validate 한지만 판단
+  완) 휴대전화와 관심 지역은 값을 입력했을 경우에만 flag를 false로 바꾸고 validation 체크
 2. 회원가입 하기 버튼 활성화 클릭 시 post 하기 전 체크해야 할 것
    - 아이디 중복 체크 여부 확인
    - 관심 지역 선택 안했을 경우 null 등으로 값 변경해서 백엔드에 넘기기 
